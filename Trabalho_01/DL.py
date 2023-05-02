f991a0fb83d58a298e95542b2c1d3d805bdc1588
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-05-02 00:58:12 -0300

    refactor: DL Main Executable updated to match new code structure

--- Trabalho_01/DL.py
+++ Trabalho_01/DL.py
@@ -2,11 +2,10 @@ import os.path
 from datetime import datetime
 
 from parser.Parser import Parser
-from lexer.Lexer import Lexer
 
 if __name__ == "__main__":
-    lexer = Lexer(file=os.path.abspath("../programs/prog_apr26_2023.dl"))
-    parser = Parser(lex=lexer)
+    file_path = os.path.abspath("programs/prog_apr26_2023.dl")
+    parser = Parser(file=file_path)
 
     parser.parse()
     print(f"{datetime.utcnow()} - Execução finalizada.")
