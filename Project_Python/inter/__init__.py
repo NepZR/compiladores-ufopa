920132fd52f7361cbd54d9119f4932d5a711090f
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-05-03 22:54:41 -0300

    feat: added inter package

---
+++ Project_Python/inter/__init__.py
@@ -0,0 +1,16 @@
+from .Node import Node
+
+from .expr.Expr import Expr
+from .expr.Bin import Bin
+from .expr.Id import Id
+from .expr.Literal import Literal
+from .expr.Or import Or
+from .expr.Rel import Rel
+
+from .stmt.Assign import Assign
+from .stmt.Block import Block
+from .stmt.Decl import Decl
+from .stmt.If import If
+from .stmt.Program import Program
+from .stmt.Stmt import Stmt
+from .stmt.Write import Write
