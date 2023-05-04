f04b54ac4d28e87794c0b8c738e31fb89e31ecc7
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-05-03 22:56:38 -0300

    patch: lexeme fixed; import refactored

--- Project_Python/lexer/Token.py
+++ Project_Python/lexer/Token.py
@@ -1,16 +1,16 @@
-from .Tag import Tag
+from lexer.Tag import Tag
 
 
 class Token:
     def __init__(self, t: Tag, lex: str) -> None:
         self.tag = t
-        self.lexeme = lex
+        self._lexeme = lex
 
     def get_tag(self) -> Tag:
         return self.tag
 
     def lexeme(self) -> str:
-        return self.lexeme
+        return self._lexeme
 
     def __str__(self) -> str:
-        return "<" + self.tag.__str__() + ", '" + self.lexeme + "'>"
+        return "<" + self.tag.__str__() + ", '" + self._lexeme + "'>"
